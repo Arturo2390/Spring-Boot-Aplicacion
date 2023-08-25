6eabb269d6f440e722b45326fdb2a5f1626b8be9
Arturo <Arturo@DESKTOP-G1UNE1K>
2023-08-25 00:28:34 -0600

    Commit inicial con NetBeans

---
+++ src/test/java/com/arturoRuiz/Aplicacion/AplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturoRuiz.Aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
